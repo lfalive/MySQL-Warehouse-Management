c06b6af18ab47152ad6decb67970f2df1ae3d4bb
Novicewu <535928879@qq.com>
2020-06-23 22:56:15 +0800

    create_trigger

--- create.py
+++ create.py
@@ -34,7 +34,7 @@ curs.execute("CREATE TABLE IF NOT EXISTS d_return("
 			 "code VARCHAR(6) NOT NULL REFERENCES d_code(code),"
 			 "return_date DATE PRIMARY KEY,"
 			 "return_number SMALLINT NULL,"
-			 "return_department SMALLINT NULL);")
+			 "return_department VARCHAR(20) NULL);")
 
 curs.execute("CREATE OR REPLACE VIEW out_return(code,date,number,department,type) AS "
 			 "SELECT code,out_date,out_number,department,'out' "
@@ -43,11 +43,37 @@ curs.execute("CREATE OR REPLACE VIEW out_return(code,date,number,department,type
 			 "SELECT code,return_date,return_number,return_department,'return' "
 			 "FROM d_return;")
 
+curs.execute(
+		"CREATE TRIGGER TRI_d_out_device "
+		"AFTER INSERT ON d_out "
+		"FOR EACH ROW "
+		"UPDATE device SET now_number = now_number - NEW.out_number WHERE code = NEW.code;")
+
+
 # test
-curs.execute("insert into d_out "
-			 "values('ag','财务','2012-03-09','刘德华',50,'张学友');")
-curs.execute("select * from out_return;")
+# curs.execute("insert into d_out "
+# 			 "values('ag','财务','2012-03-09','刘德华',50,'张学友');")
+# curs.execute("insert into d_return "
+# 			 "values('ag','2012-03-10',50,'财务');")
+
+curs.execute("INSERT INTO d_code VALUES('001','Macbook Pro');")
+curs.execute("INSERT INTO device VALUES('001',500,500);")
+# 提交之后才会保存本地
+conn.commit()
+
+curs.execute("SELECT * FROM device;")
+# curs.execute("select * from out_return;")
 results = curs.fetchall()
+print("BEFORE OUT")
+print(results)
+
+curs.execute("INSERT INTO d_out VALUES('001','财务','2020-06-09','梅西',100,'C罗');")
+# 提交
+conn.commit()
+
+curs.execute("SELECT * FROM device;")
+results = curs.fetchall()
+print("AFTER OUT")
 print(results)
 
 curs.close()
