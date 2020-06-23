9d9a072f12701c19f515335080f1d84437f2943b
Novicewu <535928879@qq.com>
2020-06-23 15:35:31 +0800

    test_conn_query

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from str import DBUser, DBPw, DB
 conn = pymysql.connect(host='localhost', port=3306, user=DBUser, passwd=DBPw, database=DB)
 curs = conn.cursor()
 
-curs.execute('SHOW DATABASES;')
+curs.execute('SHOW TABLES;')
 rows = curs.fetchall()
 
 print(rows)
