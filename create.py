c16b111ea4841ca0869f431fb7e5913b7a1bbdab
Novicewu <535928879@qq.com>
2020-06-24 11:01:58 +0800

    test_all_trigger

--- create.py
+++ create.py
@@ -64,31 +64,86 @@ curs.execute("CREATE TRIGGER TRI_d_in_device "
 			 "UPDATE device SET now_number = now_number + NEW.in_number , total_number = total_number + NEW.in_number"
 			 "				WHERE code = NEW.code;")
 
-# test
+# test view
 # curs.execute("insert into d_out "
 # 			 "values('ag','财务','2012-03-09','刘德华',50,'张学友');")
 # curs.execute("insert into d_return "
 # 			 "values('ag','2012-03-10',50,'财务');")
+# 提交之后才会保存本地
+# conn.commit()
+# curs.execute("select * from out_return;")
+# results = curs.fetchall()
+# print(results)
 
-curs.execute("INSERT INTO d_code VALUES('001','Macbook Pro');")
-curs.execute("INSERT INTO device VALUES('001',500,500);")
+# test trigger
+sql = "INSERT INTO d_code VALUES(%s,%s);"
+data = [
+    ('001', 'Macbook Pro'),
+    ('002', 'iPad Pro 2020'),
+    ('003', 'iPhone 11 Max Pro')
+]
+# 拼接并执行sql语句
+curs.executemany(sql, data)
+sql = "INSERT INTO device VALUES(%s,%s,%s);"
+data = [
+    ('001',500,500),
+    ('002',600,600),
+    ('003',600,600)
+]
+# 拼接并执行sql语句
+curs.executemany(sql, data)
 # 提交之后才会保存本地
 conn.commit()
 
-curs.execute("SELECT * FROM device;")
-# curs.execute("select * from out_return;")
+curs.execute("SELECT * FROM d_code;")
 results = curs.fetchall()
-print("BEFORE OUT")
+print("DEVICE INFO: ",end="")
+print(results)
+curs.execute("SELECT * FROM device;")
+results = curs.fetchall()
+print("DEVICE INFO ",end="")
 print(results)
 
+
+# # out
+curs.execute("SELECT * FROM device;")
+results = curs.fetchall()
+print("\nBEFORE OUT ",end="")
+print(results)
 curs.execute("INSERT INTO d_out VALUES('001','财务','2020-06-09','梅西',100,'C罗');")
 # 提交
 conn.commit()
-
 curs.execute("SELECT * FROM device;")
 results = curs.fetchall()
-print("AFTER OUT")
+print("AFTER OUT ",end="")
 print(results)
+
+# # return
+curs.execute("SELECT * FROM device;")
+results = curs.fetchall()
+print("\nBEFORE RETURN ",end="")
+print(results)
+curs.execute("INSERT INTO d_return VALUES('001','2020-06-09',100,'财务');")
+# 提交
+conn.commit()
+curs.execute("SELECT * FROM device;")
+results = curs.fetchall()
+print("AFTER RETURN ",end="")
+print(results)
+
+# # in
+curs.execute("SELECT * FROM device;")
+results = curs.fetchall()
+print("\nBEFORE IN ",end="")
+print(results)
+curs.execute("INSERT INTO d_in VALUES('001','2020-06-09','Apple',20,6999,'Tony');")
+# 提交
+conn.commit()
+curs.execute("SELECT * FROM device;")
+results = curs.fetchall()
+print("AFTER IN ",end="")
+print(results)
+
 
 curs.close()
 conn.close()
